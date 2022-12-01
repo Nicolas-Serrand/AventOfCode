07acc34898a43f3a8bed750beb2769afe81c459b
Nicolas Serrand <serrand.nicolas@gmail.com>
2022-12-01 13:39:19 +0100

    Day 1

---
+++ Day1.py
@@ -0,0 +1,31 @@
+from aoc_2022_template import *
+import collections
+import math
+#import networkx as nx
+
+DAY = 1
+
+s = get_input(DAY).strip()
+
+def part1():
+    elves = []
+    for k in s.split("\n\n"):
+        elves.append(sum(ints(k)))
+    ans = max(elves)
+    print(ans)
+    submit(DAY, 1, ans)
+    
+def part2():
+    elves = []
+    for k in s.split("\n\n"):
+        elves.append(sum(ints(k)))
+    top = []
+    for _ in range(3):
+        top.append(max(elves))
+        elves.remove(top[-1])
+    ans = sum(top)
+    submit(DAY, 2, ans)
+
+if __name__ == "__main__":
+    part1() 
+    part2()
